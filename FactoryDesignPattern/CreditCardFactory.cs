96b3a94c299186a2e488c22c73cb3175cffa313e
Oyekunle Oyewusi <sensationalkunlex@gmail.com>
2022-11-13 15:05:28 +0100

    Add project files.

---
+++ FactoryDesignPattern/CreditCardFactory.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+class CreditCardFactory
+{
+    public static CreditCard GetCreditCard(string cardType)
+    {
+        CreditCard cardDetails = null;
+        if (cardType == "MoneyBack")
+        {
+            cardDetails = new MoneyBack();
+        }
+        else if (cardType == "Titanium")
+        {
+            cardDetails = new Titanium();
+        }
+        else if (cardType == "Platinum")
+        {
+            cardDetails = new Platinum();
+        }
+        return cardDetails;
+    }
+}
---
+++ FactoryDesignPattern/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+CreditCard cardDetails = CreditCardFactory.GetCreditCard("Platinum");
+
+if (cardDetails != null)
+{
+    Console.WriteLine("CardType : " + cardDetails.GetCardType());
+    Console.WriteLine("CreditLimit : " + cardDetails.GetCreditLimit());
+    Console.WriteLine("AnnualCharge :" + cardDetails.GetAnnualCharge());
+}
+else
+{
+    Console.Write("Invalid Card Type");
+}
+Console.ReadLine();
